c39c4c5f915ba89925c7396a35e7159adf9b7590
mrFq1 <1xxbx0il0@mozmail.com>
2022-07-17 14:06:25 +0800

    Set system proxy after meta started.

--- ClashX/AppDelegate.swift
+++ ClashX/AppDelegate.swift
@@ -278,20 +278,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                 self.snifferMenuItem.state = config.sniffing ? .on : .off
             }.disposed(by: disposeBag)
 
-        if !PrivilegedHelperManager.shared.isHelperCheckFinished.value &&
-            ConfigManager.shared.proxyPortAutoSet {
-            PrivilegedHelperManager.shared.isHelperCheckFinished
-                .filter({$0})
-                .take(1)
-                .take(while: {_ in ConfigManager.shared.proxyPortAutoSet})
-                .observe(on: MainScheduler.instance)
-                .bind(onNext: { _ in
-                    SystemProxyManager.shared.enableProxy()
-                }).disposed(by: disposeBag)
-        } else if ConfigManager.shared.proxyPortAutoSet {
-            SystemProxyManager.shared.enableProxy()
-        }
-
         if !PrivilegedHelperManager.shared.isHelperCheckFinished.value {
             proxySettingMenuItem.target = nil
             PrivilegedHelperManager.shared.isHelperCheckFinished
@@ -317,6 +303,22 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         } else {
             initMetaCore()
             updateConfig(showNotification: false)
+        }
+    }
+    
+    func setupSystemData() {
+        if !PrivilegedHelperManager.shared.isHelperCheckFinished.value &&
+            ConfigManager.shared.proxyPortAutoSet {
+            PrivilegedHelperManager.shared.isHelperCheckFinished
+                .filter({$0})
+                .take(1)
+                .take(while: {_ in ConfigManager.shared.proxyPortAutoSet})
+                .observe(on: MainScheduler.instance)
+                .bind(onNext: { _ in
+                    SystemProxyManager.shared.enableProxy()
+                }).disposed(by: disposeBag)
+        } else if ConfigManager.shared.proxyPortAutoSet {
+            SystemProxyManager.shared.enableProxy()
         }
     }
 
@@ -550,6 +552,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             ConfigManager.shared.isRunning = true
             proxyModeMenuItem.isEnabled = true
             dashboardMenuItem.isEnabled = true
+            
+            setupSystemData()
         } else {
             ConfigManager.shared.isRunning = false
             proxyModeMenuItem.isEnabled = false
