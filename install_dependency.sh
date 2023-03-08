7b49bb6a25fe6c5dda0e129368458c381f3f6290
mrFq1 <1xxbx0il0@mozmail.com>
2023-03-08 12:17:47 +0800

    chore: meta rules dat

--- install_dependency.sh
+++ install_dependency.sh
@@ -27,15 +27,15 @@ rm -f ./ClashX/Resources/geoip.dat
 rm -rf ./ClashX/Resources/dashboard
 rm -f GeoLite2-Country.*
 echo "install mmdb"
-curl -LO https://github.com/Dreamacro/maxmind-geoip/releases/latest/download/Country.mmdb
+curl -LO https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/country.mmdb
 gzip Country.mmdb
 mv Country.mmdb.gz ./ClashX/Resources/Country.mmdb.gz
 echo "install geosite"
-curl -LO https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
+curl -LO https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geosite.dat
 gzip geosite.dat
 mv geosite.dat.gz ./ClashX/Resources/geosite.dat.gz
 echo "install geoip"
-curl -LO https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
+curl -LO https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geoip.dat
 gzip geoip.dat
 mv geoip.dat.gz ./ClashX/Resources/geoip.dat.gz
 echo "install dashboard"
