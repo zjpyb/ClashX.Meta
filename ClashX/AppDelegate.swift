572b50cab505227f0473fd06552d0f33bc33f2f0
mrFq1 <1xxbx0il0@mozmail.com>
2023-03-08 12:27:37 +0800

    misc: launch fail

--- ClashX/AppDelegate.swift
+++ ClashX/AppDelegate.swift
@@ -1432,7 +1432,12 @@ extension AppDelegate {
             if launch_fail_times > 3 {
                 // 发生连续崩溃
                 ConfigFileManager.backupAndRemoveConfigFile()
-                try? FileManager.default.removeItem(atPath: kConfigFolderPath + "Country.mmdb")
+				let ruleFiles = ClashResourceManager.RuleFiles
+				
+				try? FileManager.default.removeItem(atPath: kConfigFolderPath + ruleFiles.mmdb.rawValue)
+				try? FileManager.default.removeItem(atPath: kConfigFolderPath + ruleFiles.geosite.rawValue)
+				try? FileManager.default.removeItem(atPath: kConfigFolderPath + ruleFiles.geoip.rawValue)
+				
                 if let domain = Bundle.main.bundleIdentifier {
                     UserDefaults.standard.removePersistentDomain(forName: domain)
                     UserDefaults.standard.synchronize()
