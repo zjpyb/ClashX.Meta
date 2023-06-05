ba9a4ae08e70036175cba970441c2bc99b101c83
mrFq1 <1xxbx0il0@mozmail.com>
2023-06-05 22:45:13 +0800

    feat: reload dashboard

--- ClashX/ViewControllers/DashboardManagerSwiftUI.swift
+++ ClashX/ViewControllers/DashboardManagerSwiftUI.swift
@@ -6,14 +6,33 @@
 //
 
 import Cocoa
+import RxSwift
 import ClashX_Dashboard_Kit
 
 class DashboardManager: NSObject {
+	private let disposeBag = DisposeBag()
+	private var inited = false
 	static let shared = DashboardManager()
+	
+	override init() {
+	}
 	
 	var dashboardWindowController: DashboardWindowController?
 	
 	func show(_ sender: NSMenuItem) {
+		if !inited {
+			inited = true
+			NotificationCenter.default.rx.notification(.configFileChange).bind {
+				[weak self] _ in
+				self?.dashboardWindowController?.reload()
+			}.disposed(by: disposeBag)
+
+			NotificationCenter.default.rx.notification(.reloadDashboard).bind {
+				[weak self] _ in
+				self?.dashboardWindowController?.reload()
+			}.disposed(by: disposeBag)
+		}
+		
 		if dashboardWindowController == nil {
 			dashboardWindowController = DashboardWindowController.create()
 			dashboardWindowController?.onWindowClose = {
