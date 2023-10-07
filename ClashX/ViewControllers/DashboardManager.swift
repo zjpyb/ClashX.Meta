6b0da117b0bd5d0bcd3ade8279e09a2451ce227f
mrFq1 <1xxbx0il0@mozmail.com>
2023-10-07 16:11:37 +0800

    misc: swiftui target

--- ClashX/ViewControllers/DashboardManager.swift
+++ ClashX/ViewControllers/DashboardManager.swift
@@ -6,23 +6,111 @@
 //
 
 import Cocoa
+import RxSwift
+#if SwiftUI_Version
+import ClashX_Dashboard
+#endif
+
 
 class DashboardManager: NSObject {
 	static let shared = DashboardManager()
 	
+	override init() {
+	}
+	
+#if SwiftUI_Version
+	let enableSwiftUI = true
+	var useYacd = MenuItemFactory.useYacdDashboard {
+		didSet {
+			if useYacd {
+				deinitNotifications()
+				dashboardWindowController?.close()
+			} else {
+				clashWebWindowController?.close()
+			}
+		}
+	}
+	var dashboardWindowController: DashboardWindowController?
+#else
 	let enableSwiftUI = false
 	var useYacd = true
-
-	var dashboardWindowController: ClashWebViewWindowController?
+#endif
 	
+	private var disposables = [Disposable]()
+	
+	
+	var clashWebWindowController: ClashWebViewWindowController?
+
 	func show(_ sender: NSMenuItem?) {
+#if SwiftUI_Version
+		initNotifications()
+		
+		if useYacd {
+			dashboardWindowController = nil
+			showWebWindow(sender)
+		} else {
+			clashWebWindowController = nil
+			showSwiftUIWindow(sender)
+		}
+#else
+		showWebWindow(sender)
+#endif
+	}
+	
+	func showWebWindow(_ sender: NSMenuItem?) {
+		if clashWebWindowController == nil {
+			clashWebWindowController = ClashWebViewWindowController.create()
+			clashWebWindowController?.onWindowClose = {
+				[weak self] in
+				self?.clashWebWindowController = nil
+			}
+		}
+		clashWebWindowController?.showWindow(sender)
+	}
+	
+	func deinitNotifications() {
+		disposables.forEach {
+			$0.dispose()
+		}
+		disposables.removeAll()
+	}
+	
+	deinit {
+		deinitNotifications()
+	}
+}
+
+#if SwiftUI_Version
+extension DashboardManager {
+	func showSwiftUIWindow(_ sender: NSMenuItem?) {
 		if dashboardWindowController == nil {
-			dashboardWindowController = ClashWebViewWindowController.create()
+			dashboardWindowController = DashboardWindowController.create()
 			dashboardWindowController?.onWindowClose = {
 				[weak self] in
 				self?.dashboardWindowController = nil
 			}
 		}
+		
+		dashboardWindowController?.set(ConfigManager.apiUrl, secret: ConfigManager.shared.overrideSecret ?? ConfigManager.shared.apiSecret)
+		
 		dashboardWindowController?.showWindow(sender)
 	}
+	
+	func initNotifications() {
+		guard !useYacd, disposables.count == 0 else { return }
+		
+		let n1 = NotificationCenter.default.rx.notification(.configFileChange).subscribe {
+			[weak self] _ in
+			self?.dashboardWindowController?.reload()
+		}
+
+		let n2 = NotificationCenter.default.rx.notification(.reloadDashboard).subscribe {
+			[weak self] _ in
+			self?.dashboardWindowController?.reload()
+		}
+		disposables.append(n1)
+		disposables.append(n2)
+	}
+	
 }
+#endif
